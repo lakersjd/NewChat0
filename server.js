39305b5d5734d9b00abc31b03af0bf1977a9e355
lakersjd <eliasjd223@gmail.com>
2025-05-23 19:58:46 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,57 +1,28 @@
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIo(server);
 
-app.use(express.static(__dirname + "/public"));
+let usersOnline = 0;
 
-let queue = [];
+app.use(express.static('public'));
 
-io.on("connection", socket => {
-  socket.on("join", ({ country, language }) => {
-    socket.meta = { country, language };
-    let partner = queue.find(
-      s => s !== socket &&
-      s.meta.country === country &&
-      s.meta.language === language
-    );
-    if (partner) {
-      queue = queue.filter(s => s !== partner);
-      socket.partner = partner;
-      partner.partner = socket;
-      socket.emit("match", { initiator: true });
-      partner.emit("match", { initiator: false });
-    } else {
-      queue.push(socket);
-    }
-  });
+io.on('connection', (socket) => {
+    usersOnline++;
+    io.emit('userCount', usersOnline);
 
-  socket.on("offer", data => {
-    if (socket.partner) socket.partner.emit("offer", data);
-  });
-  socket.on("answer", data => {
-    if (socket.partner) socket.partner.emit("answer", data);
-  });
-  socket.on("candidate", data => {
-    if (socket.partner) socket.partner.emit("candidate", data);
-  });
-  socket.on("message", data => {
-    if (socket.partner) socket.partner.emit("message", data);
-  });
-  socket.on("skip", () => {
-    if (socket.partner) socket.partner.emit("stop");
-    socket.partner = null;
-  });
-  socket.on("stop", () => {
-    if (socket.partner) socket.partner.emit("stop");
-    socket.partner = null;
-  });
-  socket.on("report", () => {
-    console.log("User reported.");
-  });
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        usersOnline--;
+        io.emit('userCount', usersOnline);
+    });
 });
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
